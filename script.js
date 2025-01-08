529b1ae26d9c21cbe1e1a521fafab1e8e0f52898
Joshua Castillo <jcast046@odu.edu>
2025-01-08 18:57:31 -0500

    Style changes. Mobile User Friendly

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.nav-links');
 
-menuToggle.addEventListener('click', function () {
-    if (navLinks.classList.contains('open')) {
-        navLinks.classList.remove('open');
-        navLinks.classList.add('close');
-        menuToggle.classList.remove('open');
-    }
-    else {
-        navLinks.classList.remove('close');
-        navLinks.classList.add('open');
-        menuToggle.classList.add('open');
-    }
+// Toggle menu open/close
+menuToggle.addEventListener('click', () => {
+    menuToggle.classList.toggle('open');
+    navLinks.classList.toggle('open');
 });
 
 // Fade-out effect
@@ -28,14 +21,9 @@ document.querySelectorAll('a').forEach(anchor => {
 
 // Parallax scrolling effect
 function updateBackgroundPosition() {
-    // Get the scrolled distance once the scroll event is triggered
     const scrolled = window.scrollY;
-    // Adjust the background position to be 20% of the current scrolled distance
-    this.document.body.style.backgroundPositionY = -(scrolled * 0.2) + 'px';
+    document.body.style.backgroundPositionY = -(scrolled * 0.2) + 'px';
 }
 
-// Update background position as soon as the page loads,
-// rather than waiting for the first scroll event
 updateBackgroundPosition();
-
 window.addEventListener('scroll', updateBackgroundPosition);
